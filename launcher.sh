59b6ca0a3e52d26dbfee23700e44c2ce72307dd7
Gabriel Braun <gabriel@braun-mail.at>
2024-09-30 08:55:20 +0200

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,7 +5,11 @@ while ! ping -c 1 -W 1 8.8.8.8; do
 	echo "Wainting for connection..."
 	sleep 1
 done
+echo "Online..."
 cd "$(dirname "$0")"
+echo "changed Dir"
 sudo node start.js
+echo "started node backend server"
 sleep 3
 sudo node startx.js
+echo "started XServer"
