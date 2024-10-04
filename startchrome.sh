c2dba0418b873e6f8f3f8271aff22817bffba6d3
Gabriel Braun <gabriel@braun-mail.at>
2024-10-04 09:14:38 +0200

    edit default data in config

--- startchrome.sh
+++ startchrome.sh
@@ -14,7 +14,8 @@ unclutter -idle 1 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' '~/.config/chromium/Default/Preferences'
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' '~/.config/chromium/Default/Preferences'
 
-chromium-browser http://localhost:4000/ --window-size=1920,1080 --kiosk --incognito --disable-infobars >
+chromium-browser http://localhost:4000/ --window-size=1920,1080 --kiosk --incognito --disable-infobars --noerrdialogs --disable-crash-report --start-fullscreen --start-maximized --window-position=0,0 --ignore-certificate-errors --test-type
+
 
 
 
